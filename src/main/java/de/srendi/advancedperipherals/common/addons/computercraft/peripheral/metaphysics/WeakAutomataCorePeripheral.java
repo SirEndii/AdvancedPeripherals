bd49e5a8173bc23cda05bd90f942b85364ea5fc8
zyxkad <zyxkad@gmail.com>
2024-12-26 17:53:00 -0700

    add mountToShip for automata turtles

--- src/main/java/de/srendi/advancedperipherals/common/addons/computercraft/peripheral/metaphysics/WeakAutomataCorePeripheral.java
+++ src/main/java/de/srendi/advancedperipherals/common/addons/computercraft/peripheral/metaphysics/WeakAutomataCorePeripheral.java
@@ -2,14 +2,28 @@ package de.srendi.advancedperipherals.common.addons.computercraft.peripheral.met
 
 import dan200.computercraft.api.turtle.ITurtleAccess;
 import dan200.computercraft.api.turtle.TurtleSide;
+import de.srendi.advancedperipherals.common.addons.APAddons;
 import de.srendi.advancedperipherals.common.addons.computercraft.operations.AutomataCoreTier;
 import de.srendi.advancedperipherals.common.addons.computercraft.peripheral.plugins.*;
 import de.srendi.advancedperipherals.common.configuration.APConfig;
 import de.srendi.advancedperipherals.lib.metaphysics.IAutomataCoreTier;
 import de.srendi.advancedperipherals.lib.peripherals.AutomataCorePeripheral;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
 public class WeakAutomataCorePeripheral extends AutomataCorePeripheral {
     public static final String TYPE = "weak_automata";
+    private static final List<Function<AutomataCorePeripheral, IPeripheralPlugin>> PERIPHERAL_PLUGINS = new ArrayList<>();
+
+    static {
+        PERIPHERAL_PLUGINS.add(AutomataItemSuckPlugin::new);
+        PERIPHERAL_PLUGINS.add(AutomataLookPlugin::new);
+        PERIPHERAL_PLUGINS.add(AutomataBlockHandPlugin::new);
+        PERIPHERAL_PLUGINS.add(AutomataSoulFeedingPlugin::new);
+        PERIPHERAL_PLUGINS.add(AutomataChargingPlugin::new);
+    }
 
     public WeakAutomataCorePeripheral(ITurtleAccess turtle, TurtleSide side) {
         this(TYPE, turtle, side, AutomataCoreTier.TIER1);
@@ -22,6 +36,9 @@ public class WeakAutomataCorePeripheral extends AutomataCorePeripheral {
         addPlugin(new AutomataBlockHandPlugin(this));
         addPlugin(new AutomataSoulFeedingPlugin(this));
         addPlugin(new AutomataChargingPlugin(this));
+        if (APAddons.vs2Loaded) {
+            addPlugin(new AutomataVSMountPlugin(this));
+        }
     }
 
     @Override
