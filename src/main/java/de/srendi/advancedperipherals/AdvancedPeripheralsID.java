bf28d18af3d03315b6baa96237d38690de28788e
zyxkad <zyxkad@gmail.com>
2025-01-11 12:39:12 -0700

    update display name

--- src/main/java/de/srendi/advancedperipherals/AdvancedPeripheralsID.java
+++ src/main/java/de/srendi/advancedperipherals/AdvancedPeripheralsID.java
@@ -2,6 +2,7 @@ package de.srendi.advancedperipherals;
 
 import net.minecraftforge.fml.common.Mod;
 
+// This empty mod is used to only register ID for handling chunk tickets
 @Mod(AdvancedPeripherals.ITEM_MOD_ID)
 public class AdvancedPeripheralsID {
 }
