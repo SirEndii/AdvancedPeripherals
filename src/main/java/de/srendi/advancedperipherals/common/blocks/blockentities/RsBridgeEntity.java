ddb4d2cada27d5d8cbe31aa6141a918bf22ec566
Srendi <info@denizdemiroglu.de>
2025-01-14 22:45:07 +0100

    RS Bridge can now connect to a RS System

--- src/main/java/de/srendi/advancedperipherals/common/blocks/blockentities/RsBridgeEntity.java
+++ src/main/java/de/srendi/advancedperipherals/common/blocks/blockentities/RsBridgeEntity.java
@@ -1,26 +1,38 @@
 package de.srendi.advancedperipherals.common.blocks.blockentities;
 
+import com.refinedmods.refinedstorage.api.network.impl.node.SimpleNetworkNode;
+import com.refinedmods.refinedstorage.api.network.node.NetworkNode;
+import com.refinedmods.refinedstorage.common.api.support.network.ConnectionStrategy;
 import com.refinedmods.refinedstorage.common.api.support.network.InWorldNetworkNodeContainer;
 import com.refinedmods.refinedstorage.common.api.support.network.NetworkNodeContainerProvider;
+import com.refinedmods.refinedstorage.common.support.network.InWorldNetworkNodeContainerImpl;
+import com.refinedmods.refinedstorage.common.support.network.SimpleConnectionStrategy;
 import de.srendi.advancedperipherals.common.addons.computercraft.peripheral.RSBridgePeripheral;
 import de.srendi.advancedperipherals.common.blocks.base.PeripheralBlockEntity;
+import de.srendi.advancedperipherals.common.configuration.APConfig;
 import de.srendi.advancedperipherals.common.setup.BlockEntityTypes;
 import de.srendi.advancedperipherals.lib.peripherals.IPeripheralTileEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Set;
 
 public class RsBridgeEntity extends PeripheralBlockEntity<RSBridgePeripheral> implements IPeripheralTileEntity, NetworkNodeContainerProvider {
 
     protected CompoundTag peripheralSettings;
+    private final InWorldNetworkNodeContainer networkNodeContainer;
 
     public RsBridgeEntity(BlockPos pos, BlockState state) {
         super(BlockEntityTypes.RS_BRIDGE.get(), pos, state);
         peripheralSettings = new CompoundTag();
+        ConnectionStrategy connectionStrategy = new SimpleConnectionStrategy(pos);
+        NetworkNode node = new SimpleNetworkNode(APConfig.PERIPHERALS_CONFIG.rsConsumption.get());
+        networkNodeContainer = new InWorldNetworkNodeContainerImpl(this, node, "RS Bridge", 1, connectionStrategy, null);
     }
 
     @NotNull
@@ -42,7 +54,7 @@ public class RsBridgeEntity extends PeripheralBlockEntity<RSBridgePeripheral> im
     @NotNull
     @Override
     public Set<InWorldNetworkNodeContainer> getContainers() {
-        return Set.of();
+        return Set.of(networkNodeContainer);
     }
 
     @Override
@@ -50,7 +62,22 @@ public class RsBridgeEntity extends PeripheralBlockEntity<RSBridgePeripheral> im
     }
 
     @Override
-    public boolean canBuild(ServerPlayer serverPlayer) {
+    public boolean canBuild(@NotNull ServerPlayer serverPlayer) {
         return true;
+    }
+
+    @Override
+    public void update(@Nullable Level level) {
+        NetworkNodeContainerProvider.super.update(level);
+    }
+
+    @Override
+    public void initialize(@Nullable Level level, @Nullable Runnable callback) {
+        NetworkNodeContainerProvider.super.initialize(level, callback);
+    }
+
+    @Override
+    public void remove(@Nullable Level level) {
+        NetworkNodeContainerProvider.super.remove(level);
     }
 }
